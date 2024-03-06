ff016e1b9889a0535b85ae5aad6ee849db4de524
liujeru <jeru.token@gmail.com>
2024-03-06 18:30:24 +0800

    reverse merge short title

--- split_test.py
+++ split_test.py
@@ -1,7 +1,26 @@
-sentences = ["This", "is", "an", "example", "list", "of", "sentences"]
-title_index = 2
-next_index = 5
+import pandas
+from paragraph_cutter import ParagraphCutter
+import sys
 
-del sentences[title_index:next_index]
 
-print(sentences)
+domestic_file_path = 'data/domestic_report.xlsx'
+industry_file_path = 'data/industry_report.xlsx'
+
+df = pandas.read_excel(domestic_file_path)
+df = df.head(30)
+df = df[['source', 'content']]
+
+
+sys.stdout = open('output/domestic_report_split_output.txt', 'wt')
+
+for index, row in df.iterrows():
+    pc = ParagraphCutter(row['content'])
+    chunks = pc.cut_paragraph()
+
+    print(f"Source: {row['source']}")
+    print("===========================")
+
+    for i, chunk in enumerate(chunks):
+        print(f"Chunk #{i} - Length: {len(chunk)}")
+        print(chunk.strip())
+    print("****************************\n\n")
