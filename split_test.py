55131e2b2f2ec82ca55c7c0d73d30f1820a5bb48
liujeru <jeru.token@gmail.com>
2024-03-07 10:45:01 +0800

    Change combined logic

--- split_test.py
+++ split_test.py
@@ -5,13 +5,15 @@ import sys
 
 domestic_file_path = 'data/domestic_report.xlsx'
 industry_file_path = 'data/industry_report.xlsx'
+news_file_path = 'data/news.xlsx'
 
-df = pandas.read_excel(domestic_file_path)
+df = pandas.read_excel(industry_file_path)
 df = df.head(30)
 df = df[['source', 'content']]
 
-
-sys.stdout = open('output/domestic_report_split_output_95_0_30.txt', 'wt')
+sys.stdout = open('output/industry_report_split_output_95_0_30.txt', 'wt')
+#sys.stdout = open('output/domestic_report_split_output_95_0_30.txt', 'wt')
+#sys.stdout = open('output/news_split_output_95_0_30.txt', 'wt')
 
 for index, row in df.iterrows():
     pc = ParagraphCutter(row['content'])
